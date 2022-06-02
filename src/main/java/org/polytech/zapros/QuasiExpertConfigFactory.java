ec1d5f19b8c5cec775c0c2c2c928b683b5dfab9a
ADanielGhost <a.d.ghost@mail.ru>
2022-06-02 03:31:06 +0300

    threshold2 v.1.4.7

--- src/main/java/org/polytech/zapros/QuasiExpertConfigFactory.java
+++ src/main/java/org/polytech/zapros/QuasiExpertConfigFactory.java
@@ -1,13 +1,26 @@
 package org.polytech.zapros;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.polytech.zapros.bean.Assessment;
 import org.polytech.zapros.bean.Criteria;
 import org.polytech.zapros.bean.QuasiExpertConfig;
 import org.polytech.zapros.service.util.QuasiExpertConfigInitUtils;
 
 public class QuasiExpertConfigFactory {
     public static QuasiExpertConfig getConfig(List<Criteria> criteriaList) {
-        return QuasiExpertConfigInitUtils.init(criteriaList);
+        List<Criteria> criteriaListSorted = criteriaList.stream()
+            .peek(criteria -> {
+                List<Assessment> assessments = criteria.getAssessments().stream()
+                    .sorted(Comparator.comparing(Assessment::getOrderId))
+                    .collect(Collectors.toList());
+                criteria.setAssessments(assessments);
+            })
+            .sorted(Comparator.comparing(Criteria::getOrderId))
+            .collect(Collectors.toList());
+
+        return QuasiExpertConfigInitUtils.init(criteriaListSorted);
     }
 }
--- src/main/java/org/polytech/zapros/bean/QuasiExpertConfig.java
+++ src/main/java/org/polytech/zapros/bean/QuasiExpertConfig.java
@@ -2,6 +2,7 @@ package org.polytech.zapros.bean;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+@AllArgsConstructor
 @RequiredArgsConstructor
 public class QuasiExpertConfig {
 
