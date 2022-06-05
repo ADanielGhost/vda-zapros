89a36c5710e02777bffc60225cff7d3d9582aac3
ADanielGhost <a.d.ghost@mail.ru>
2022-06-05 20:21:57 +0300

    answer v.1.6.2.1

--- src/main/java/org/polytech/zapros/bean/Answer.java
+++ src/main/java/org/polytech/zapros/bean/Answer.java
@@ -1,5 +1,6 @@
 package org.polytech.zapros.bean;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@EqualsAndHashCode
 public class Answer {
 
     public enum AnswerType { BETTER, WORSE, EQUAL }
